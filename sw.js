5739a5dfff8e23315291b48049f109c007da27b3
Tiago Scheidecker <scheidecker.restelli@gmail.com>
2026-08-15 16:30:44 +0200

    8 regioni: aggiunte Liguria e Friuli-Venezia Giulia

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
    Una cache per la shell (cambia a ogni build) e una per ogni regione scaricata
    (cambia solo col dataset), cosi' "elimina la mappa X" e' selettivo e un fix di
    codice non fa riscaricare niente. */
-const APP = 'copilota-app-v43';
-const REGIONS = ["valle_aosta", "trentino_alto_adige", "umbria", "marche", "lazio", "abruzzo"];
+const APP = 'copilota-app-v44';
+const REGIONS = ["valle_aosta", "liguria", "trentino_alto_adige", "friuli_venezia_giulia", "umbria", "marche", "lazio", "abruzzo"];
 const DATA_PREFIX = 'copilota-data-';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './regions.json',
                './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
